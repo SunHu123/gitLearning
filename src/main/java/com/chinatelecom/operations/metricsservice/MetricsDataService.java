b97b135d3df301d59272348c1c184737aa908fb2
lichao <lichao@HF-OA37003.ahdxitcloud.com>
2021-12-05 23:27:46 +0800

    增加测试用例

--- src/main/java/com/chinatelecom/operations/metricsservice/MetricsDataService.java
+++ src/main/java/com/chinatelecom/operations/metricsservice/MetricsDataService.java
@@ -29,50 +29,10 @@ public class MetricsDataService implements IWorkService{
     
 	private final static Logger logger=LogManager.getLogger(MetricsDataService.class);
 	
-	private ConcurrentHashMap<String, TypeHttpRequest> waitUsers=null;
 	
-	public ConcurrentHashMap<String, TypeHttpRequest> getWaitUsers() {
-		return waitUsers;
-	}
-
 	public MetricsDataService() {
-		waitUsers=new ConcurrentHashMap<>();
 	}
 	
-	public IDataResponse publish(String content) {
-		final JSONObject jsonResult=new JSONObject();
-		if(content.length()>10000) {
-			jsonResult.put("result", 2);
-			jsonResult.put("message", "too large");
-		}
-		else {
-			ShareData addDataDAO = DAOFactory.getDAOObject(ShareData.class);
-			String id = UUID.randomUUID().toString().substring(1, 28);
-			ILoginUserInfo userInfo = WebContextHolder.getLoginUserInfo();
-			try {
-				addDataDAO.setModified("create_time", "now()");
-				addDataDAO.setDataid(id);
-				addDataDAO.setContent(content);
-				addDataDAO.setCreator(userInfo.getEmpeeAcct());
-				addDataDAO.doInsert(true,true);
-				jsonResult.put("result", 0);
-				jsonResult.put("id", id);
-				TypeHttpRequest waitUserRequest = waitUsers.get(userInfo.getEmpeeAcct());
-				if(waitUserRequest!=null) {
-					JSONObject jsonMessage=new JSONObject();
-					jsonMessage.put("content", content);
-					waitUserRequest.asyncResponse(new JSONResponse(jsonMessage));
-				}				
-			} catch (SQLException | IOException e) {
-				logger.error(e.getMessage(), e);
-				jsonResult.put("result", 1);
-				jsonResult.put("message", e.getMessage());
-			}
-		}
-		JSONResponse response=new JSONResponse(jsonResult);
-		return response;
-	}
-
 	/**
 	 * 显示最新的数据
 	 * @param startindex 
@@ -81,27 +41,9 @@ public class MetricsDataService implements IWorkService{
 	 */
 	public IDataResponse list(int startIndex, int length)  {
 		JSONObject jsonResult = new JSONObject();
-		ShareData queryDataDAO=DAOFactory.getDAOObject(ShareData.class);
-		try {
-			ILoginUserInfo userInfo = WebContextHolder.getLoginUserInfo();
-			queryDataDAO.setSelected(new String[] {"create_time","content"});
-			queryDataDAO.addDesc("create_time");
-			queryDataDAO.andEqual("creator",new StringExpression(userInfo.getEmpeeAcct(), true));
-			ArrayList<ShareData> queryResult=queryDataDAO.doQuery(startIndex, length);
-			JSONArray jsonPages=new JSONArray();
-			for(ShareData data: queryResult) {
-				JSONObject jsonPage=new JSONObject();
-				jsonPage.put("createtime",DateTimeHelper.formatDate(data.getCreate_time().getTime(),"Y-m-d H:M:S"));
-				jsonPage.put("content",data.getContent());
-				jsonPages.put(jsonPage);
-			}
-			jsonResult.put("result", 0);
-			jsonResult.put("data", jsonPages);
-		} catch (SQLException e) {
-			logger.error(e.getMessage(),e);
-			jsonResult.put("result", 1);
-			jsonResult.put("message", e.getMessage());
-		}
+		ILoginUserInfo userInfo = WebContextHolder.getLoginUserInfo();
+		jsonResult.put("result", 0);
+		jsonResult.put("data", userInfo.getStaffName());
 		return new JSONResponse(jsonResult);
 	}
 	
